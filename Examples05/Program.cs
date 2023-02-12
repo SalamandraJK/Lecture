bc14d8d4852108b3d78ed1c68efaeb4a6286082a
MikhailPL <muke.jry@gmail.com>
2023-02-12 19:34:47 +0300

    Users name individual

--- Examples05/Program.cs
+++ Examples05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Individual greeting
+
+Console.Write("Enter your uzername: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАРИЯ!");
+}
+
+else{
+
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
